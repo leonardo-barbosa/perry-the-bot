bb761558e277887c65d11427ed9b169e4870152d
Leonardo Barbosa <leonardo.barbosa@pagar.me>
2017-09-25 17:08:59 -0300

    Added supporter Jonatas

--- Dicty/agents.py
+++ Dicty/agents.py
@@ -1,6 +1,6 @@
 ASSIGN = [
     {'name': 'Leonardo Barbosa', 'email': 'leonardo.barbosa@pagar.me', 'slack_id': 'U6BL8MYF8'},
     {'name': 'Murilo Souza', 'email':  'murilo.souza@pagar.me', 'slack_id': 'U53QKBQSD'},
-    {'name': 'Vagner Santana', 'email':  'vagner.tambolo@pagar.me', 'slack_id': 'U6J7HTSSC'}
-    # {'name': 'Henrique Kano', 'email':  'henrique.kano@pagar.me', 'slack_id': 'U2K8R1MD4'} NOT ASSIGN ANYMORE
+    {'name': 'Vagner Santana', 'email':  'vagner.tambolo@pagar.me', 'slack_id': 'U6J7HTSSC'},
+    {'name': 'Jonatas Maximiano', 'email': 'jonatas.maximiano@pagar.me', 'slack_id': 'U6WF284KC'}
 ]
