8030a949f000f9332a2764afe5a643045a0b8921
Leonardo Barbosa <leonardo.barbosa@pagar.me>
2018-01-05 09:36:36 -0200

    change the time of schedule tasks

--- perry_the_bot.py
+++ perry_the_bot.py
@@ -10,11 +10,11 @@ sched = BlockingScheduler()
 def timed_job():
     Perry.assign_tickets()
 
-@sched.scheduled_job('interval', days=1)
+@sched.scheduled_job('cron', hour=22, day_of_week='0-6')
 def timed_job():
     Perry.tag_yesterday_tickets()
 
-@sched.scheduled_job('interval', minutes=120)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour='7-19/2')
 def timed_job():
     Perry.notify_pending_interaction_tickets()
 
